685351dba42d15305cac6895232a211c579c3464
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2020-05-21 22:20:43 +0800

    update to db_ideas

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    roles = db.relationship('Role', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}> and '.format(self.username) + '<Email {}>'.format(self.email) 
