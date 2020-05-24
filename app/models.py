e68b0c0ef06518fbda619d375191a3425e2cff4e
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2020-05-25 01:49:13 +0800

    db with feedback and long answers result implemented

--- app/models.py
+++ app/models.py
@@ -14,13 +14,14 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # admin = db.Column(db.Boolean, default = False)
     quiz = db.relationship('Quiz', backref='usersesh', lazy='dynamic')
     feedback = db.relationship('Feedbacks', backref='feedback_user', lazy='dynamic')
     long_answer = db.relationship('Long_Answers', backref='long_answer_user', lazy='dynamic')
 
 
     def __repr__(self):
-        return '<User {}> and '.format(self.username) + '<Email {}>'.format(self.email)
+        return 'Username {}'.format(self.username)
     # generate a hash for the password to be encoded 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -32,20 +33,28 @@ class Quiz(db.Model):
     result = db.Column(db.Integer, index=True, default = '0')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    def __repr__(self):
+        return 'short answer quiz status for {}: '.format(self.usersesh.username)
 
 class Question (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question_body = db.Column(db.Text, index=True)
     long_question = db.Column(db.Boolean, default = False)
+    mark_for_question = db.Column(db.Integer, index = True)
     options = db.relationship('Option', backref='question', lazy='dynamic')
-    long_answer = db.relationship('Long_Answers', uselist= False, backref='long_question')
+    long_answer = db.relationship('Long_Answers', backref='long_question', lazy='dynamic')
 
+    def __repr__(self):
+        return 'Question {}: '.format(self.id) + '{}'.format(self.question_body)
 
 class Option (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     option_body = db.Column(db.Text, index=True)
     correct = db.Column(db.Boolean, default = False, nullable=False)
     question_id =db.Column(db.Integer, db.ForeignKey('question.id'))
+
+    def __repr__(self):
+        return 'Option: {}: '.format(self.option_body)
 
 class Feedbacks (db.Model):
     id = db.Column(db.Integer, primary_key=True)
